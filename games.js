ac0efde1149493e1f502a9c73c7adc1d01c13a4b
FalloutX <inderjitsidhu@outlook.com>
2015-02-07 14:26:28 +0530

    Added Game Creating Functionality

---
+++ games.js
@@ -0,0 +1,39 @@
+Games = new Mongo.Collection("games");
+
+
+/*
+
+game = {
+  currentTurn: [],
+  deck: [],
+  table: [],
+  players:{
+    a:{
+      hand :[],
+      pile :[],
+      score :{}
+    },
+    b: {}
+  }
+  inProgress: true/false,
+  started: date,
+  finished: date,
+  winner: id
+}
+
+*/
+
+if (Meteor.isServer){
+  Meteor.publish("games", function(){
+    return Games.find({currentTurn: this.userId});
+  });
+
+  Meteor.publish("users", function(){
+    return Meteor.users.find();
+  });
+}
+
+if (Meteor.isClient){
+  Meteor.subscribe("games");
+  Meteor.subscribe("users");
+}

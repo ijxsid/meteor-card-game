9d4c6d3788e0419cce1aa03df2b3cb091ca74805
FalloutX <inderjitsidhu@outlook.com>
2015-02-09 01:42:38 +0530

    Added Scoring Logic/ Still some bugs

--- games.js
+++ games.js
@@ -44,5 +44,39 @@ Meteor.methods({
     var game = GameFactory.createGame([Meteor.userId(), otherPlayerId]);
     Games.insert(game);
     console.log("Game has been created.!");
+  },
+  takeTurn: function(gameId, id, card){
+    var game = Games.findOne({_id: gameId});
+    var hand = game.players[id].hand;
+
+    if (game.currentTurn[0] !== id && !Turns.inHand(hand, card)) return;
+
+    var match = Turns.getMatch(card, game.table);
+
+    if(match) {
+      Turns.takeMatch(game, id, card, match);
+    } else{
+      game.table.push(card);
+    }
+
+    game.players[id].hand = Turns.removeCard(card, hand);
+    game.currentTurn.unshift(game.currentTurn.pop());
+
+    if( allHandsEmpty(game.players)){
+      if( game.deck.length > 0){
+        GameFactory.dealPlayers(game.players, game.deck);
+      }else{
+        scoreGame(game);
+      }
+    }
+    Games.update(gameId, game);
   }
 });
+
+
+function allHandsEmpty(players){
+  return _.every(players, function(player){
+    return player.hand.length === 0;
+  });
+}
+
